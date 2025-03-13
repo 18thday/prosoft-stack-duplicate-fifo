d90dc1b9f1c800c55f20d68c6c5e95472cfa2e25
18thday <day18th@yandex.ru>
2025-03-13 15:03:31 +0500

    first realisation of stack

---
+++ stack_duplicate_fifo.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+
+
+template <typename T>
+class StackDuplicateFifo
+{
+public:
+    StackDuplicateFifo() = default;
+    StackDuplicateFifo(const StackDuplicateFifo&) = default;
+    StackDuplicateFifo& operator=(const StackDuplicateFifo&) = default;
+
+    StackDuplicateFifo(StackDuplicateFifo&& other) :
+        m_stack(std::move(other.m_stack)),
+        m_valueToCount(std::move(other.m_valueToCount)),
+        m_size(other.m_size)
+    {
+        other.Clear();
+    }
+
+    StackDuplicateFifo& operator=(StackDuplicateFifo&& other)
+    {
+        m_stack = std::move(other.m_stack);
+        m_valueToCount = std::move(other.m_valueToCount);
+        m_size = other.m_size;
+        other.Clear();
+        return *this;
+    }
+
+    T& Top() noexcept
+    {
+        RemoveInvalid();
+        return m_stack.back();
+    }
+    const T& Top() const noexcept
+    {
+        RemoveInvalid();
+        return m_stack.back();
+    }
+
+    bool Empty() const noexcept
+    {
+        return m_size == 0;
+    }
+    size_t Size() const noexcept
+    {
+        return m_size;
+    }
+
+    void Push(const T& value)
+    {
+        m_stack.push_back(value);
+        ++m_valueToCount[m_stack.back()];
+        ++m_size;
+    }
+    void Push(T&& value)
+    {
+        m_stack.push_back(std::move(value));
+        ++m_valueToCount[m_stack.back()];
+        ++m_size;
+    }
+
+    void Pop()
+    {
+        if (m_size == 0)
+        {
+            return;
+        }
+        auto it = m_valueToCount.find(m_stack.back());
+        while (it == m_valueToCount.end())
+        {
+            m_stack.pop_back();
+            it = m_valueToCount.find(m_stack.back());
+        }
+        if (--(it->second) == 0)
+        {
+            m_valueToCount.erase(it);
+            m_stack.pop_back();
+        }
+        --m_size;
+    }
+
+private:
+    std::deque<T> m_stack;
+    std::unordered_map<T, size_t> m_valueToCount;
+    size_t m_size = 0;
+
+    void RemoveInvalid()
+    {
+        while (!m_valueToCount.count(m_stack.back()) && !m_stack.empty())
+        {
+            m_stack.pop_back();
+        }
+    }
+
+    void Clear()
+    {
+        m_size = 0;
+        m_stack.clear();
+        m_valueToCount.clear();
+    }
+};
