ad7cf59f5caad06145e048e6bb53699664d05baa
18thday <day18th@yandex.ru>
2025-03-14 13:07:22 +0500

    second stack realisation with list and unordered_map

--- stack_duplicate_fifo.h
+++ stack_duplicate_fifo.h
@@ -2,111 +2,112 @@
 
 #include <deque>
 #include <iostream>
+#include <list>
 #include <queue>
 #include <unordered_map>
-
 
 template <typename T>
 class StackDuplicateFifo
 {
 public:
     StackDuplicateFifo() = default;
-    StackDuplicateFifo(const StackDuplicateFifo&) = default;
-    StackDuplicateFifo& operator=(const StackDuplicateFifo&) = default;
+
+    StackDuplicateFifo(const StackDuplicateFifo& other) : m_stack(other.m_stack)
+    {
+        for (auto it = m_stack.begin(), it_end = m_stack.end(); it != it_end; ++it)
+        {
+            m_valueToDuplicateIter[*it].push(it);
+        }
+    }
+
+    StackDuplicateFifo& operator=(const StackDuplicateFifo& other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        StackDuplicateFifo tmp(other);
+        this->Swap(tmp);
+        return *this;
+    }
 
     StackDuplicateFifo(StackDuplicateFifo&& other) :
         m_stack(std::move(other.m_stack)),
-        m_valueToCount(std::move(other.m_valueToCount)),
-        m_size(other.m_size)
+        m_valueToDuplicateIter(std::move(other.m_valueToDuplicateIter))
     {
         other.Clear();
     }
 
     StackDuplicateFifo& operator=(StackDuplicateFifo&& other)
     {
-        if (this == &other) {
+        if (this == &other)
+        {
             return *this;
         }
         m_stack = std::move(other.m_stack);
-        m_valueToCount = std::move(other.m_valueToCount);
-        m_size = other.m_size;
+        m_valueToDuplicateIter = std::move(other.m_valueToDuplicateIter);
         other.Clear();
         return *this;
     }
 
+    ~StackDuplicateFifo() = default;
+
     T& Top()
     {
-        RemoveInvalid();
         return m_stack.back();
     }
     const T& Top() const
     {
-        RemoveInvalid();
         return m_stack.back();
     }
 
     bool Empty() const noexcept
     {
-        return m_size == 0;
+        return m_stack.empty();
     }
     size_t Size() const noexcept
     {
-        return m_size;
+        return m_stack.size();
     }
 
     void Push(const T& value)
     {
         m_stack.push_back(value);
-        ++m_valueToCount[m_stack.back()];
-        ++m_size;
+        m_valueToDuplicateIter[m_stack.back()].push(--m_stack.end());
     }
     void Push(T&& value)
     {
         m_stack.push_back(std::move(value));
-        ++m_valueToCount[m_stack.back()];
-        ++m_size;
+        m_valueToDuplicateIter[m_stack.back()].push(--m_stack.end());
     }
 
     void Pop()
     {
-        if (m_size == 0)
+        if (m_stack.size() == 0)
         {
             return;
         }
-        auto it = m_valueToCount.find(m_stack.back());
-        while (it == m_valueToCount.end())
+        auto it = m_valueToDuplicateIter.find(m_stack.back());
+        m_stack.erase(it->second.front());
+        it->second.pop();
+        if (it->second.empty())
         {
-            m_stack.pop_back();
-            it = m_valueToCount.find(m_stack.back());
-        }
-        if (--(it->second) == 0)
-        {
-            m_valueToCount.erase(it);
-            m_stack.pop_back();
-        }
-        if (--m_size == 0 && !m_stack.empty())
-        {
-            m_stack.clear();
+            m_valueToDuplicateIter.erase(it);
         }
     }
-
 private:
-    std::deque<T> m_stack;
-    std::unordered_map<T, size_t> m_valueToCount;
-    size_t m_size = 0;
+    std::list<T> m_stack;
+    std::unordered_map<T, std::queue<typename std::list<T>::iterator> > m_valueToDuplicateIter;
 
-    void RemoveInvalid()
+    void Swap(StackDuplicateFifo& other)
     {
-        while (!m_stack.empty() && !m_valueToCount.count(m_stack.back()))
-        {
-            m_stack.pop_back();
-        }
+        std::swap(m_stack, other.m_stack);
+        std::swap(m_valueToDuplicateIter, other.m_valueToDuplicateIter);
     }
 
     void Clear()
     {
-        m_size = 0;
         m_stack.clear();
-        m_valueToCount.clear();
+        m_valueToDuplicateIter.clear();
     }
 };
